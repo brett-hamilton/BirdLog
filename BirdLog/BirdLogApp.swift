752151e189bcc678e5c08a3024a7ac8318d4ee1c
Brett Hamilton <bretthamilton024@gmail.com>
2025-05-02 20:37:46 -0400

    Initial Commit

---
+++ BirdLog/BirdLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdLogApp.swift
+//  BirdLog
+//
+//  Created by Brett Hamilton on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BirdLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
