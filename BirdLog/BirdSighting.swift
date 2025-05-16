06a7af7016cd1141fb9f7226bae3d96b36473617
Brett Hamilton <bretthamilton024@gmail.com>
2025-05-16 17:17:03 -0400

    Add BirdSighting model

---
+++ BirdLog/BirdSighting.swift
@@ -0,0 +1,15 @@
+//
+//  BirdSighting.swift
+//  BirdLog
+//
+//  Created by Brett Hamilton on 5/16/25.
+//
+
+import Foundation
+
+struct BirdSighting {
+    var name: String
+    var date: Date
+    var location: String?
+    var notes: String?
+}
